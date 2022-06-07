9fc7e5da518decf006209f38209650ff9e85f302
houdesampath <sampath.kumar@wadhwaniai.org>
2022-06-07 14:16:51 +0530

    reading data from database

--- blog/main.py
+++ blog/main.py
@@ -16,10 +16,15 @@ def get_db():
     finally:
         db.close
 
-@app.post('/blog')
+@app.post('/blog', status_code=201)
 def create(request: schemas.Blog, db : Session = Depends(get_db)) :
     new_blog = models.Blog(title=request.title, body = request.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+@app.get('/blog') 
+def all(db : Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
