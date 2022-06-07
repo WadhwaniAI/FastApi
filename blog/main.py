cb3c953dc11a876fe27ad1ef5fbf66e6f8b2daae
houdesampath <sampath.kumar@wadhwaniai.org>
2022-06-07 14:24:53 +0530

    filtering items from id

--- blog/main.py
+++ blog/main.py
@@ -1,5 +1,7 @@
+from ast import In
+from os import stat
 from turtle import title
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, status
 import models, schemas
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -15,9 +17,11 @@ def get_db():
         yield db
     finally:
         db.close
+        
 
-@app.post('/blog', status_code=201)
-def create(request: schemas.Blog, db : Session = Depends(get_db)) :
+
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
+def create(request: schemas.Blog, db: Session = Depends(get_db)) :
     new_blog = models.Blog(title=request.title, body = request.body)
     db.add(new_blog)
     db.commit()
@@ -25,6 +29,11 @@ def create(request: schemas.Blog, db : Session = Depends(get_db)) :
     return new_blog
 
 @app.get('/blog') 
-def all(db : Session = Depends(get_db)):
+def all(db):
     blogs = db.query(models.Blog).all()
-    return blogs
+    return blogs 
+
+@app.get('/blog/{id}')
+def getBlog(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    return blog
