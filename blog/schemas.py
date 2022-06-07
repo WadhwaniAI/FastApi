1e266d6495cbf2b13141575cf0b30285b78e8e25
houdesampath <sampath.kumar@wadhwaniai.org>
2022-06-07 18:19:44 +0530

    using ResponseModel
    used to show only req data

--- blog/schemas.py
+++ blog/schemas.py
@@ -6,3 +6,9 @@ from pydantic import BaseModel
 class Blog(BaseModel):
     title: str
     body: str
+        
+class ShowBlog(BaseModel):
+    title: str
+    
+    class Config():
+        orm_mode = True
