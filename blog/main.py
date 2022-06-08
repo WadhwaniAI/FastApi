9c7130d2b67f078463d74b053995c2ad8223a1c0
houdesampath <sampath.kumar@wadhwaniai.org>
2022-06-08 13:02:03 +0530

    doc tags
    used for classification of routes

--- blog/main.py
+++ blog/main.py
@@ -18,7 +18,7 @@ def get_db():
         
 
 
-@app.post('/blog', status_code=status.HTTP_201_CREATED)
+@app.post('/blog', status_code=status.HTTP_201_CREATED, tags=['blogs'])
 def create(request: schemas.Blog, db: Session = Depends(get_db)) :
     new_blog = models.Blog(title=request.title, body = request.body)
     db.add(new_blog)
@@ -26,17 +26,17 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)) :
     db.refresh(new_blog)
     return new_blog
 
-@app.get('/blog') 
+@app.get('/blog',tags=['blogs']) 
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs 
 
-@app.get('/blogsList', status_code=status.HTTP_200_OK, response_model=List[schemas.ShowBlog])
+@app.get('/blogsList', status_code=status.HTTP_200_OK, response_model=List[schemas.ShowBlog],tags=['blogs'])
 def showListOfBlogsTitle(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog,tags=['blogs'])
 def getBlog(id, response: Response,db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
@@ -45,7 +45,7 @@ def getBlog(id, response: Response,db: Session = Depends(get_db)):
         #return {'Detail': f'Blog with id {id} is not available'}
     return blog
 
-@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT,tags=['blogs'])
 def delete(id, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
@@ -54,7 +54,7 @@ def delete(id, db: Session = Depends(get_db)):
     db.commit()
     return 'deleted'
           
-@app.put('/blog/{id}')
+@app.put('/blog/{id}',tags=['blogs'])
 def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
@@ -64,7 +64,7 @@ def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
     return {'Blog updated' : request}
 
 
-@app.post('/user', status_code=status.HTTP_201_CREATED, response_model=schemas.ShowUser)
+@app.post('/user', status_code=status.HTTP_201_CREATED, response_model=schemas.ShowUser, tags=['user'])
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
     new_user = models.User(name=request.name, email=request.email, password=hashing.Hash.bcrypt(request.password))
     db.add(new_user)
@@ -72,7 +72,7 @@ def create_user(request: schemas.User, db: Session = Depends(get_db)):
     db.refresh(new_user)
     return new_user
 
-@app.post('/user/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowUser)
+@app.post('/user/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowUser, tags=['user'])
 def get_user(id, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == id).first()
     if not user:
