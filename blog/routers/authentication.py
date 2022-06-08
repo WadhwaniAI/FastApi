985819446bd86094c36c6550672374f4f72ef9ed
houdesampath <sampath.kumar@wadhwaniai.org>
2022-06-08 16:43:10 +0530

    added authentication
    auth using email and password

---
+++ blog/routers/authentication.py
@@ -0,0 +1,22 @@
+import schemas, database, models, hashing
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+
+router = APIRouter(
+    prefix='/auth',
+    tags=['authentication']
+)
+
+get_db = database.get_db
+
+@router.post('/login')
+def login(request: schemas.Login, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.email == request.username).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f'User with username {request.username} not found.' )
+    
+    if not hashing.Hash.verify(user.password  ,request.password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail='Incorrect Password')
+    
+    return 'Logged In'
